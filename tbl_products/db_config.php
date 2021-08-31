a62326ba52c7ac9cfab7d479e89d22f24dfdf626
Agah Nata <76636470+arduino-uno@users.noreply.github.com>
2021-08-31 11:56:07 +0700

    Delete tbl_products directory
    Delete tbl_products directory

--- tbl_products/db_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Set Database Config
-define( "DB_HOST", "localhost" );
-define( "DB_USERNAME", "root" );
-define( "DB_PASSWORD", "" );
-define( "DB_DATABASE_NAME", "foodiadb" );
-define( "MYSQL_CONN_ERROR", "Unable to connect to database." );
